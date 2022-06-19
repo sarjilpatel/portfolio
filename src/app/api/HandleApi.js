5ac5331ced79997687216a19b490732ac80d16af
HENISH PATEL <81601957+henishpatel9045@users.noreply.github.com>
2022-06-19 16:56:54 +0530

    Update HandleApi.js

--- src/app/api/HandleApi.js
+++ src/app/api/HandleApi.js
@@ -1,7 +1,7 @@
 import { create } from "apisauce";
 
 const api = create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: "https://sarjilportfoliobackend.pythonanywhere.com/",
 });
 
 export default api;
